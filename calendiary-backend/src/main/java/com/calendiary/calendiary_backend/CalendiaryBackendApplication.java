5211838bbbc696dc3d870b2f78155f72fb65bf31
01304814 <e1304814@student.tuwien.ac.at>
2025-05-17 13:25:56 +0200

    Initial commit

---
+++ calendiary-backend/src/main/java/com/calendiary/calendiary_backend/CalendiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.calendiary.calendiary_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendiaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendiaryBackendApplication.class, args);
+	}
+
+}
---
+++ calendiary-backend/src/test/java/com/calendiary/calendiary_backend/CalendiaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendiary.calendiary_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendiaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
